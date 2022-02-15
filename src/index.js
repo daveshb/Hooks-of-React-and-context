85b3eecbc9392b99a31bfabe21ce898bf236e00e
David Henao <daves.hb@hotmail.com>
2022-02-15 10:40:56 -0500

    Uso de useState en CounterApp

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { CounterAPP } from './components/01-useState/CounterAPP';
+import { HookApp } from './HookApp';
+
+
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    <CounterAPP />,
+
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
